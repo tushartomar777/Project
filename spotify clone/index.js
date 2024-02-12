aa786214383ae6c4a105479220e5344a77ae7164
TUSHAR SINGH TOMAR <122335039+tushartomar777@users.noreply.github.com>
2024-02-12 14:32:25 +0530

    Add files via upload

---
+++ spotify clone/index.js
@@ -0,0 +1,11 @@
+
+
+document.querySelector(".fa-plus").addEventListener("mouseenter", e => {
+    document.querySelector(".floating").style.display = "block";
+})
+
+
+
+document.querySelector(".fa-plus").addEventListener("mouseleave", e => {
+    document.querySelector(".floating").style.display = "none";
+})
